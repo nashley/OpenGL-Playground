c8fe235553ee45746de1f0aeaf9355091af65f17
yentup <californiajack06@gmail.com>
2014-11-18 23:31:36 -0500

    Added controls for user to control camera Also fixed cube colors

--- src/main.c
+++ src/main.c
@@ -11,10 +11,101 @@
 
 // Global vars
 GLFWwindow* window;
+vec3 pos = {0, 5, -0.5};
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
+void calc_matrices(mat4x4 M) {
+	static double last_time = -1;
+	if (last_time == -1)
+		last_time = glfwGetTime();
+	double curr_time = glfwGetTime();
+	float delta_time = (float) (curr_time - last_time);
+
+	static float hang = M_PI; // Horizontal angle (toward -Z)
+	static float vang = 0.0f; // Vertical angle (0 at the horizon)
+	static float fov = M_PI / 4.0f; // 45 deg
+
+	static float speed = 4.0f; // 4 units / second
+	static float mouse_speed = 0.004f;
+
+	static double xpos, ypos;
+
+	glfwGetCursorPos(window, &xpos, &ypos);
+	glfwSetCursorPos(window, 1024/2, 768/2); // Reset mouse
+
+	// New orientation
+	hang += mouse_speed * (float) (1024/2 - xpos);
+	vang += mouse_speed * (float) ( 768/2 - ypos);
+
+	vec3 dir = {
+		cosf(vang) * sinf(hang),
+		sinf(vang),
+		cosf(vang) * cosf(hang)
+	},
+	right = {
+		sinf(hang - M_PI / 2.0f),
+		0,
+		cosf(hang - M_PI / 2.0f)
+	};
+
+	vec3 up;
+	vec3_mul_cross(up, right, dir);
+
+	vec3 temp;
+	// Move forward
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		vec3_scale(temp, dir, delta_time * speed);
+		vec3_add(pos, pos, temp);
+	}
+	// Move backward
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		vec3_scale(temp, dir, delta_time * speed);
+		vec3_sub(pos, pos, temp);
+	}
+	// Strafe right
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		vec3_scale(temp, right, delta_time * speed);
+		vec3_add(pos, pos, temp);
+	}
+	// Strafe left
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		vec3_scale(temp, right, delta_time * speed);
+		vec3_sub(pos, pos, temp);
+	}
+	// Move up
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+		vec3_scale(temp, up, delta_time * speed);
+		vec3_add(pos, pos, temp);
+	}
+	// Move down
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+		vec3_scale(temp, up, delta_time * speed);
+		vec3_sub(pos, pos, temp);
+	}
+
+	mat4x4 projection, view, model;
+	mat4x4_perspective(
+		projection,
+		fov,         // in radians
+		4.0f / 3.0f, // 4:3 ratio
+		0.1f,        // display range : 0.1 unit <-> 100 units
+		100.0f
+	);
+	vec3 center;
+	vec3_add(center, pos, dir);
+	mat4x4_look_at(
+		view,
+		pos,    // eye
+		center, // center
+		up      // up
+	);
+	mat4x4_identity(model);
+
+	// projection * view * model
+	mat4x4 tempM;
+	mat4x4_mul(tempM, projection, view);
+	mat4x4_mul(M, tempM, model);
+
+	last_time = curr_time;
 }
 
 int main() {
@@ -37,7 +128,6 @@ int main() {
 	}
 
 	glfwMakeContextCurrent(window);
-	glfwSetKeyCallback(window, key_callback);
 
 	glewExperimental = 1; // Need this for core profile
 	if (glewInit() != GLEW_OK) {
@@ -48,9 +138,16 @@ int main() {
 
 	// Pre loop
 	////////
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	glfwSetCursorPos(window, 1024/2, 768/2);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glEnable(GL_DEPTH_TEST); // Enable depth test
 	glDepthFunc(GL_LESS); // Accept frag if closer to camera than former
+	// glEnable(GL_CULL_FACE);
+		// Cull triangles that arent towards camera
+		// Disables noclip but is faster
 
 	GLuint vert_arr;
 	glGenVertexArrays(1, &vert_arr);
@@ -105,141 +202,63 @@ int main() {
 
 	// Color : vertex (randomly generated)
 	GLfloat colors[] = {
-		0.583f,  0.771f,  0.014f,
-		0.609f,  0.115f,  0.436f,
-		0.327f,  0.483f,  0.844f,
-
-		0.822f,  0.569f,  0.201f,
-		0.435f,  0.602f,  0.223f,
-		0.310f,  0.747f,  0.185f,
-
-		0.435f,  0.602f,  0.223f,
-		0.822f,  0.569f,  0.201f,
-		0.359f,  0.583f,  0.152f,
-
-		0.483f,  0.596f,  0.789f,
+		0.483f,  0.596f,  0.789f,//
 		0.195f,  0.548f,  0.859f,
 		0.559f,  0.861f,  0.639f,
-
-		0.483f,  0.596f,  0.789f,
+		0.435f,  0.602f,  0.223f,//
+		0.822f,  0.569f,  0.201f,
+		0.359f,  0.583f,  0.152f,
+		0.327f,  0.483f,  0.844f,//
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.435f,  0.602f,  0.223f,//
+		0.310f,  0.747f,  0.185f,
+		0.822f,  0.569f,  0.201f,
+		0.483f,  0.596f,  0.789f,//
 		0.559f,  0.861f,  0.639f,
 		0.771f,  0.328f,  0.970f,
-
+		0.327f,  0.483f,  0.844f,//
 		0.676f,  0.977f,  0.133f,
 		0.583f,  0.771f,  0.014f,
-		0.327f,  0.483f,  0.844f,
 
-		// ^ 6 v
-
-		0.997f,  0.513f,  0.064f,
+		0.997f,  0.513f,  0.064f,//
 		0.945f,  0.719f,  0.592f,
 		0.543f,  0.021f,  0.978f,
-
-		0.279f,  0.317f,  0.505f,
+		0.279f,  0.317f,  0.505f,//
 		0.167f,  0.620f,  0.077f,
 		0.347f,  0.857f,  0.137f,
-
-		0.167f,  0.620f,  0.077f,
+		0.167f,  0.620f,  0.077f,//
 		0.279f,  0.317f,  0.505f,
 		0.783f,  0.290f,  0.734f,
-
-		0.722f,  0.645f,  0.174f,
+		0.722f,  0.645f,  0.174f,//
 		0.225f,  0.587f,  0.040f,
 		0.302f,  0.455f,  0.848f,
-
-		0.722f,  0.645f,  0.174f,
+		0.722f,  0.645f,  0.174f,//
 		0.302f,  0.455f,  0.848f,
 		0.053f,  0.959f,  0.120f,
-
-		0.673f,  0.211f,  0.457f,
+		0.673f,  0.211f,  0.457f,//
 		0.997f,  0.513f,  0.064f,
 		0.543f,  0.021f,  0.978f
 	};
-	// GLfloat colors[] = {
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-	// 	0.0f,  0.0f,  0.0f,
-
-	// 	// ^ 6 v
-
-	// 	0.0f,  1.0f,  1.0f, // 1.1
-	// 	0.0f,  0.5f,  1.0f, // 1.3
-	// 	1.0f,  0.0f,  1.0f, // 1.2
-
-	// 	0.0f,  1.0f,  0.0f, // 2.1
-	// 	0.0f,  0.0f,  1.0f, // 2.2
-	// 	1.0f,  0.0f,  0.0f, // 2.3
-
-	// 	0.0f,  0.0f,  1.0f, // 2.2
-	// 	0.0f,  1.0f,  0.0f, // 2.1
-	// 	1.0f,  0.0f,  0.0f, // 2.4
-
-	// 	1.0f,  0.0f,  0.0f, // 3.1
-	// 	0.0f,  0.0f,  1.0f, // 3.3
-	// 	0.0f,  1.0f,  0.0f, // 3.2
-
-	// 	1.0f,  0.0f,  0.0f, // 3.1
-	// 	0.0f,  1.0f,  0.0f, // 3.2
-	// 	0.0f,  0.0f,  1.0f, // 3.4
-
-	// 	0.5f,  0.0f,  1.0f, // 1.4
-	// 	0.0f,  1.0f,  1.0f, // 1.1
-	// 	1.0f,  0.0f,  1.0f  // 1.2
-	// };
 	GLuint color_buff;
 	glGenBuffers(1, &color_buff);
 	glBindBuffer(GL_ARRAY_BUFFER, color_buff);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
-	mat4x4 projection, view, model;
-	mat4x4_perspective(
-		projection,
-		M_PI / 4.0f, // 45 deg fov
-		4.0f / 3.0f, // 4:3 ratio
-		0.1f,        // display range : 0.1 unit <-> 100 units
-		100.0f
-	);
-	mat4x4_look_at(
-		view,
-		(vec3) {4.0f, 3.0f, 3.0f}, // eye
-		(vec3) {0.0f, 0.0f, 0.0f}, // center
-		(vec3) {0.0f, 1.0f, 0.0f}  // up
-	);
-	mat4x4_identity(model);
-
-	// projection * view * model
-	mat4x4 temp, MVP;
-	mat4x4_mul(temp, projection, view);
-	mat4x4_mul(MVP, temp, model);
-
 	GLuint matrix = glGetUniformLocation(prog, "MVP");
+	mat4x4 MVP;
 	////////
 
-	while (!glfwWindowShouldClose(window)) {
+	while (
+		!glfwWindowShouldClose(window) &&
+		glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS
+	) {
 		// Render
 		////////
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram(prog); // Use shaders
+
+		calc_matrices(MVP);
 		glUniformMatrix4fv(matrix, 1, GL_FALSE, &MVP[0][0]);
 
 		glEnableVertexAttribArray(0);
